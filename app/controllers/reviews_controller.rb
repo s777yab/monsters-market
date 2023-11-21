1054e02ddc35504f55d8989afabec4a5adcb419a
Tony Masek <tm@tony-masek.com>
2023-11-21 16:27:43 +0000

    Fix bug in simple form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,19 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @monster = Monster.find(:monster_id)
+    @review.monster = @monster
+    @review.user = current_user
+    if @review.save
+      render "monsters/show", status: 200
+    else
+      render "monsters/show", status: 422
+    end
+  end
 
+  private
 
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
 end
