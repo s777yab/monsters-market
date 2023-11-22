6969a4202385d79007ef7249c0e05bce78310c9b
Tony Masek <tm@tony-masek.com>
2023-11-22 18:57:02 +0000

    Create validations for presence of attributes for monster

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -4,6 +4,8 @@ class Monster < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
+  validates :name, :species, :ability, :price, :address, presence: true
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
