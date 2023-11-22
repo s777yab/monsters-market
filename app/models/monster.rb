ef0f373c306ac34ecc362f4500c328f7388fdf2c
Tony Masek <tm@tony-masek.com>
2023-11-22 12:28:40 +0000

    Fix bug in geocoder add geocoder

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -3,4 +3,7 @@ class Monster < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
