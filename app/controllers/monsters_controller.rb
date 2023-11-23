2a076b3d065d43a6752ca18cc900b9eb269f9519
Tony Masek <tm@tony-masek.com>
2023-11-23 12:56:02 +0000

    Fix bug on show page

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,11 +1,9 @@
 class MonstersController < ApplicationController
   before_action :set_monster, only: %i[show edit update destroy]
   before_action :monster_params, only: [:create]
-  before_action :bookable?, only: [:show]
 
   def index
     @monsters = near_and_bookable
-
 
     # The `geocoded` scope filters only monsters with coordinates
     @markers = @monsters.map do |monster|
