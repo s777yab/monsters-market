32d95437a3de23f5da8e48a4d5834e857a492500
Tony Masek <tm@tony-masek.com>
2023-11-24 13:29:43 +0000

    Add tracking to dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def user
+    @monsters = Monster.where(user_id: current_user.id)
+    # The `geocoded` scope filters only flats with coordinates
+    @markers_tracking = @monsters.map do |monster|
+    {
+      lat: monster.latitude,
+      lng: monster.longitude,
+      popup_monster_html: render_to_string(partial: "tracking_popup", locals: { monster: monster })
+    }
+    end
   end
 end
